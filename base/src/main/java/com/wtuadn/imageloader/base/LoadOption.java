4254266afd61537143daf81a4b6e3aa8462b8c71
wtuadn <wtuadn@gmail.com>
2018-03-09 17:44:56 +0800

    添加部分参数

--- base/src/main/java/com/wtuadn/imageloader/base/LoadOption.java
+++ base/src/main/java/com/wtuadn/imageloader/base/LoadOption.java
@@ -1,8 +1,30 @@
 package com.wtuadn.imageloader.base;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
+
 /**
  * Created by wtuadn on 2018/2/10.
  */
 
 public class LoadOption {
+    public static int DISK_CACHE_DEFAULT = 0;
+    public static int DISK_CACHE_NONE = 1;
+    public static int DISK_CACHE_ALL = 2;
+    public static int DISK_CACHE_SOURCE = 3;
+    public static int DISK_CACHE_RESULT = 4;
+
+    public Context context;
+    public String url;
+    public int placeholderResId = Integer.MIN_VALUE;
+    public Drawable placeholderDrawable;
+    public int errorResId = Integer.MIN_VALUE;
+    public Drawable errorDrawable;
+    public int diskCache = DISK_CACHE_DEFAULT;
+    public ImageView.ScaleType scaleType;
+    public boolean skipMemory;
+    public boolean autoPlay = true;//如果是gif的话自动播放
+    public boolean asBitmap;
+    // TODO: 2018/3/9 添加统一对bitmap变换的接口
 }
--- picassoloader/src/main/java/com/wtuadn/imageloader/picassoloader/PicassoLoader.java
+++
@@ -1,9 +0,0 @@
-package com.wtuadn.imageloader.picassoloader;
-
-/**
- * Created by wtuadn on 2018/2/10.
- */
-
-public class PicassoLoader {
-
-}
