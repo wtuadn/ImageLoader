9b6b7d94cf93b0fc9f77244452941b7002ba0c05
wtuadn <wtuadn@gmail.com>
2018-05-23 11:23:59 +0800

    更新proguard

--- app/src/test/java/com/wtuadn/imageloader/ExampleUnitTest.java
+++ app/src/test/java/com/wtuadn/imageloader/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.wtuadn.imageloader;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.util.Stack;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,6 +12,26 @@ import static org.junit.Assert.assertEquals;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+//        assertEquals(4, 2 + 2);
+
+        Stack<Integer> test = new Stack<Integer>();
+        test.push(1);
+        test.push(2);
+        test.push(3);
+        test.push(4);
+        test.push(5);
+        reverse(test);
+        while (!test.isEmpty()) {
+            System.out.println(test.pop());
+        }
     }
+
+    private void reverse(Stack<Integer> test) {
+        if (!test.isEmpty()){
+            Integer i = test.pop();
+            reverse(test);
+            test.push(i);
+        }
+    }
+
 }
